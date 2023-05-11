b0a4b5263a422f9413109c71a9ace0188dffa70c
kushalshrestha <kushalshr@gmail.com>
2023-05-11 01:19:24 -0500

    feat : CRUD endpoints for Student created

--- src/main/java/com/kushalshrestha/springboot/controller/StudentController.java
+++ src/main/java/com/kushalshrestha/springboot/controller/StudentController.java
@@ -1,9 +1,8 @@
 package com.kushalshrestha.springboot.controller;
 
 import com.kushalshrestha.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,8 +26,36 @@ public class StudentController {
         return students;
     }
 
+
     @GetMapping("/students/{id}/{firstName}/{lastName}")
     public Student studentPathVariable(@PathVariable("id") int studentId, @PathVariable String firstName, @PathVariable String lastName) {
         return new Student(studentId, firstName, lastName);
+    }
+
+    @GetMapping("/students/query")
+    public Student studentRequestVariable(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName) {
+        return new Student(id, firstName, lastName);
+    }
+
+    @PostMapping("/students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    @PutMapping("students/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    @DeleteMapping("students/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId) {
+        System.out.println(studentId);
+        return "Student deleted successfully!!";
     }
 }
